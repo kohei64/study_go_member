962cad1981977b945c3e85618823dff76546aa0d
kohei64 <kohei3400@icloud.com>
2022-05-13 22:09:16 +0900

    --allow-empty-message

--- router/router.go
+++ router/router.go
@@ -19,11 +19,16 @@ func Init() {
 	e.Use(middleware.Recover())
 
 	// ルーター
-	e.GET("/users",controller.GetUsers) //全ユーザー取得
-	e.GET("/user/:id", controller.GetUser) //ユーザー取得
-	e.POST("/user", controller.CreateUser) //ユーザー作成
-	e.PUT("/user/:id", controller.UpdateUser) //ユーザー編集
-	e.DELETE("/user/:id", controller.DeleteUser) //ユーザー削除
+	e.POST("/login", controller.Login)
+
+	r:=e.Group("/restricted")
+	r.Use(middleware.JWTWithConfig(controller.Config))
+	
+	r.GET("/users",controller.GetUsers) //全ユーザー取得
+	r.GET("/user/:id", controller.GetUser) //ユーザー取得
+	r.POST("/user", controller.CreateUser) //ユーザー作成
+	r.PUT("/user/:id", controller.UpdateUser) //ユーザー編集
+	r.DELETE("/user/:id", controller.DeleteUser) //ユーザー削除
 
 	e.Logger.Fatal(e.Start("localhost:8080"))
 }
