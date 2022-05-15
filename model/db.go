76f6b0e504a0fd4b4c82024093192a33cb6122a6
kohei64 <kohei3400@icloud.com>
2022-05-15 20:38:37 +0900

    --allow-emptu-message

--- model/db.go
+++ model/db.go
@@ -21,7 +21,7 @@ func init() {
 
 	pass:=os.Getenv("PASSWORD")
 
-	dsn := "root:"+pass+"!@tcp(127.0.0.1:5432)/study_go_member?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:"+pass+"@tcp(127.0.0.1:5432)/study_go_member?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("database can't connect")
