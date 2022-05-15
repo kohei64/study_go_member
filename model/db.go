8b03e3bbc84e0b3e542d35d417831ee1f83b1d99
kohei64 <kohei3400@icloud.com>
2022-05-15 20:06:48 +0900

    add: env

--- model/db.go
+++ model/db.go
@@ -2,9 +2,11 @@ package model
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
@@ -12,7 +14,14 @@ var err error
 
 // データベース接続
 func init() {
-	dsn := "root:Mmky1031!@tcp(127.0.0.1:3306)/study_go_member?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("env error")
+	}
+
+	pass:=os.Getenv("PASSWORD")
+
+	dsn := "root:"+pass+"!@tcp(127.0.0.1:5432)/study_go_member?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("database can't connect")
