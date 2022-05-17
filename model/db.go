7d9b375731987b31d1ee27653a94befe0c1c6bf8
kohei64 <kohei3400@icloud.com>
2022-05-17 17:25:11 +0900

    add:db稼働待ち

--- model/db.go
+++ model/db.go
@@ -1,11 +1,12 @@
 package model
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
+    "time"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
 )
 
 var DB *gorm.DB
@@ -14,17 +15,24 @@ var err error
 // データベース接続
 func init() {
 
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_HOST"),
-		os.Getenv("MYSQL_DATABASE"),
-	)
+    dsn := fmt.Sprintf(
+        "%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+        os.Getenv("MYSQL_USER"),
+        os.Getenv("MYSQL_PASSWORD"),
+        os.Getenv("MYSQL_HOST"),
+        os.Getenv("MYSQL_DATABASE"),
+    )
 
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("database can't connect")
-	}
-	DB.AutoMigrate(&User{})
+    // 稼働待ち
+    for i := 0; i < 10; i++ {
+        DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+        if err == nil {
+						DB.AutoMigrate(&User{})
+            fmt.Printf(os.Getenv("DATABASE_URL") + "### connect.\n")
+            break
+        }
+        fmt.Printf("### failed to connect database. connect again.\n")
+        time.Sleep(3 * time.Second)
+    }
+
 }
